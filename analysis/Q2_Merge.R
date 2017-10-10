13c87ea1bad9ef2320afd935653892dd9518fa95
Eric McCandless <emccandless@smu.edu>
2017-10-09 21:04:12 -0400

    adding folder back

---
+++ analysis/Q2_Merge.R
@@ -0,0 +1,3 @@
+Beers <- read.csv("./data/Beers.csv", header=TRUE)
+Breweries <- read.csv("./data/Breweries.csv", header=TRUE)
+All_Data <- merge(Beers, Breweries, by.x = "Brewery_id", by.y = "Brew_ID", all.x=TRUE)
